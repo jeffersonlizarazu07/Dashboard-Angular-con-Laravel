07cb2b40527940a144a1dd6f0d4322230ea7b746
Jefferson Lizarazu <Jeffersonlizarazu@hotmail.com>
2026-03-13 00:38:48 -0500

    refactor(api): refactor files in api routes and bootstrap

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -6,42 +6,28 @@ use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\CheckRole;
 use App\Http\Middleware\SanitizeInput;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-    web: __DIR__.'/../routes/web.php',
-    api: __DIR__.'/../routes/api.php',
-    commands: __DIR__.'/../routes/console.php',
-    health: '/up',
-    then: function () {
-        Route::middleware('api')
-            ->prefix('api/auth')
-            ->group(base_path('routes/auth.php'));
-
-        Route::middleware('api')
-            ->prefix('api/users')
-            ->group(base_path('routes/users.php'));
-
-        Route::middleware('api')
-            ->prefix('api/products')
-            ->group(base_path('routes/products.php'));
-    },
-)
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up',
+    )
     ->withMiddleware(function (Middleware $middleware) {
-        // Middleware de Sanctum para cookies
         $middleware->statefulApi();
 
         $middleware->use([
-        \Illuminate\Http\Middleware\HandleCors::class,
-    ]);
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
 
-        // Registrar alias de middleware personalizados
         $middleware->alias([
             'role'     => CheckRole::class,
             'sanitize' => SanitizeInput::class,
         ]);
 
-        // Aplicar sanitización globalmente a la API
+        $middleware->prependToGroup('api', EnsureFrontendRequestsAreStateful::class);
         $middleware->appendToGroup('api', SanitizeInput::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+()
+Route::prefix('auth')->group(function () {
+    require __DIR__.'/auth.php';
+});
+
+Route::prefix('products')->group(function () {
+    require __DIR__.'/products.php';
+});
+
+Route::prefix('users')->group(function () {
+    require __DIR__.'/users.php';
+});
